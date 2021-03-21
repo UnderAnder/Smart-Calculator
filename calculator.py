3c2efec0b3b55b1f2ba24d1d7880ca94803a0a34
UnderAnder <andryushin.alexander@gmail.com>
2021-03-21 18:36:00 +0700

    Stage 2/7: 2+2+

--- calculator.py
+++ calculator.py
@@ -1,6 +1,12 @@
 
 def main():
-    print(sum(int(x) for x in input().split()))
+    while True:
+        user_input = input()
+        if user_input == '/exit':
+            print('Bye!')
+            exit()
+        if user_input:
+            print(sum(int(x) for x in user_input.split()))
 
 
 if __name__ == '__main__':
